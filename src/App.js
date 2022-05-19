ba7bb5bc5ef4d168aeb7d22ab9347ec8f48b8389
Rubèn Nieto <ruben.np@me.com>
2022-05-19 17:25:41 +0200

    Started Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Container from 'react-bootstrap/Container';
+import { Container, Nav, Navbar, Button, Row } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <Container>
-      <h1>Hello!</h1>
-    </Container>  
+    <> 
+      <Navbar bg="light" styicky="top" expand="lg" className="px-4">
+        <Navbar.Brand href="#">Brand</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>  
+            <Nav.Link href="#">Registre</Nav.Link>
+            <Button variant="primary">Login</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container className="my-4">
+        <Row className="justify-content-center">
+          <h1>Vull iniciar un negoci a Barcelona!</h1>
+          <h2>Busca el millor local</h2>
+        </Row>
+      </Container>
+    </>
   );
 };
 
